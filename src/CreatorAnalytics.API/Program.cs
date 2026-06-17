5de6bebfbec566a40ade715746531edce4602628
abdulrahmanhossam <abdulrahmn398@outlook.com>
2026-06-17 15:47:08 +0300

    feat: add Scalar.AspNetCore package and configure API reference mapping

--- src/CreatorAnalytics.API/Program.cs
+++ src/CreatorAnalytics.API/Program.cs
@@ -2,6 +2,7 @@ using CreatorAnalytics.Core.Interfaces;
 using CreatorAnalytics.Infrastructure.Data;
 using CreatorAnalytics.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
